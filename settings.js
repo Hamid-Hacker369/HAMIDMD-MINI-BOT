0bea445ab62a480602c4da25cae1d6c811a1cade
Hamid-Hacker369 <ahmadabdulhameed5490@gmail.com>
2026-08-21 03:25:28 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝙃𝘼𝙈𝙄𝘿𝙈𝘿𝘽𝙊𝙏',
+  author: '‎',
+  botName: " 𝙃𝘼𝙈𝙄𝘿𝙈𝘿𝘽𝙊𝙏 ",
+  botOwner: '𝙃𝘼𝙈𝙄𝘿𝙈𝘿𝘽𝙊𝙏 💫😶🥵', // Your name
+  ownerNumber: '923134270199', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "THIS BOT CREATED BY 𝙃𝘼𝙈𝙄𝘿𝙈𝘿𝘽𝙊𝙏💫😶🥵",
+  version: "3.0.7",
+  updateZipUrl: "",
+};
+
+module.exports = settings;
